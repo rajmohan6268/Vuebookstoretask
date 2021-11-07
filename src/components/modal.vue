c75145896de3f6197ce985d186e68b8dbecd2edd
rajmohan6268 <rajmohan6268@gmail.com>
2021-11-07 09:48:59 +0530

    feat(modal): modaal component

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -1,29 +1,29 @@
 <template>
   <div @click="$emit('close')" class="">
     <transition name="modal">
-      <div class="fixed top-0 left-0 z-50 w-full h-full modal-mask">
-        <div class="modal-wrapper">
+      <div
+        class="fixed top-0 left-0 z-50 w-full h-full transition-all duration-1000 ease-in-out delay-1000 modal-mask"
+      >
+        <div class="text-left modal-wrapper">
           <div
             @click.stop
-            class="w-full max-w-lg p-5 m-auto transition-all bg-white rounded-lg shadow-md "
+            class="w-full max-w-2xl p-2 m-auto bg-white rounded-lg shadow-md"
           >
-            <div class="modal-header">
+            <div class="relative ">
+              <button @click="$emit('close')" class="absolute right-4 top-4">
+                <font-awesome-icon class="w-10" icon="times" />
+              </button>
               <slot name="header"> default header </slot>
             </div>
 
-            <div class="m-2">
+            <div class="my-1 overflow-y-scroll body">
               <slot name="body"> default body </slot>
             </div>
 
-            <div class="modal-footer">
+            <div class="">
               <slot name="footer">
                 default footer
-                <button
-                  class="float-right modal-default-button"
-                  @click="$emit('close')"
-                >
-                  OK
-                </button>
+                <button class="float-right" @click="$emit('close')">OK</button>
               </slot>
             </div>
           </div>
@@ -39,11 +39,14 @@ export default {};
 
 <style scoped>
 .modal-mask {
-  background-color: rgba(0, 0, 0, 0.027);
+  background-color: rgba(0, 0, 0, 0.253);
   display: table;
   transition: opacity 0.3s ease;
 }
-
+.body {
+   height: fit-content;
+    max-height: 700;
+}
 .modal-wrapper {
   display: table-cell;
   vertical-align: middle;
