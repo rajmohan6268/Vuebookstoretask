d2ad31b15711e93ab304b3fab593fc71db4d62ed
rajmohan6268 <rajmohan6268@gmail.com>
2021-11-07 10:50:11 +0530

    configue vuex

--- src/main.js
+++ src/main.js
@@ -1,15 +1,41 @@
-
-import './index.css'
+import "./index.css";
 import router from "./router";
-import { createApp } from 'vue'
-import  App from './App.vue'
+import { createApp } from "vue";
+import store from "./../store/";
+import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {faAd ,faSignOutAlt,faSearch,faPlus ,faTimes ,faTimesCircle,faMinus, faPhone,faEye,faEyeSlash,faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAd,
+  faSignOutAlt,
+  faSearch,
+  faPlus,
+  faTimes,
+  faTimesCircle,
+  faMinus,
+  faPhone,
+  faEye,
+  faEyeSlash,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faPhone,faSignOutAlt ,faSearch,faTimes,faTimesCircle ,faMinus ,faEye ,faEyeSlash ,faPhone,faTrash,faAd,faPlus);
 
+library.add(
+  faPhone,
+  faSignOutAlt,
+  faSearch,
+  faTimes,
+  faTimesCircle,
+  faMinus,
+  faEye,
+  faEyeSlash,
+  faPhone,
+  faTrash,
+  faAd,
+  faPlus
+);
 
-createApp(App).use(router)
-.component("font-awesome-icon", FontAwesomeIcon)
-.mount('#app')
+createApp(App)
+  .use(router, store)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
