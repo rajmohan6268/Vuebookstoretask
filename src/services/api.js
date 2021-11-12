f64e4610e64fac70d4b84504d0afbfd033e2a0c9
rajmohan6268 <rajmohan6268@gmail.com>
2021-11-12 20:29:59 +0530

    change api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
- // baseURL: "https://rajdev-vuebookstoreapi.herokuapp.com/api",
- baseURL: "http://localhost:3000/api",
+  baseURL: "https://rajdev-vuebookstoreapi.herokuapp.com/api",
+ //baseURL: "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
